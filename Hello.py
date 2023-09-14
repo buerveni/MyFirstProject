40381713e4e8a93ebb56505326e97230f6ecccce
buerveni <christoph.buervenich@t-online.de>
2023-09-14 18:37:19 +0200

    Hello.py wurde kommentiert und aif einen Branch gelegt.

--- Hello.py
+++ Hello.py
@@ -1,1 +1,6 @@
+"""
+Dies ist ein Hello World Program.
+Zuerst wird die Funktion 'print' aufgerufen.	
+In die runde Klammern wird dann eingetragen, was ausgegeben werden soll.
+"""
 print("Hello World!")
