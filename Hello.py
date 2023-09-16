7d6320afa9f451c8a00e75cb7ca283a8eb04ddaf
buerveni <christoph.buervenich@t-online.de>
2023-09-16 08:14:24 +0200

    Aus einer neuen Welt!

--- Hello.py
+++ Hello.py
@@ -3,4 +3,5 @@ Dies ist ein Hello World Program.
 Zuerst wird die Funktion 'print' aufgerufen.	
 In die runde Klammern wird dann eingetragen, was ausgegeben werden soll.
 """
-print("Hello World!")
+print("New World!")
+print("Aus einer neuen Welt.")
